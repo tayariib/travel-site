22daaa530fbdbe933fec209fc356f21116fa8b6f
ibrahim <tayariib@gmail.com>
2018-02-17 16:20:09 +0100

    newgulp file and some modifications on styles

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require("gulp");
+var watch = require("gulp-watch");
+var postcss = require("gulp-postcss");
+var autoprefixer = require ("autoprefixer");
+var cssvars = require("postcss-simple-vars");
+var nested = require("postcss-nested");
+
+gulp.task ("default", function (){
+    console.log("wooooo, you created Gulb task.")});
+
+gulp.task ("html", function (){
+    console.log("do useful things with html.")});
+
+gulp.task ("styles", function (){
+    return gulp.src("./app/assets/styles/styles.css")
+        .pipe(postcss([cssvars,nested,autoprefixer]))
+        .pipe(gulp.dest("./app/temp/styles"));
+});
+
+
+gulp.task ("watch", function (){
+    watch("./app/index.html", function(){
+    
+    gulp.start("html");
+});
+    
+    watch("./app/assets/styles/**/*.css", function(){
+        gulp.start("styles");
+    });
+    
+                               
+                               });
