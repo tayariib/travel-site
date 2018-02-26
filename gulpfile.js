c61297109accfd8cbeca981b3f0747683831f910
ibrahim <tayariib@gmail.com>
2018-02-26 17:01:16 +0100

    progress to lec17

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,9 @@ var gulp = require("gulp"),
     autoprefixer = require("autoprefixer"),
     cssvars = require("postcss-simple-vars"),
     nested = require("postcss-nested"),
-    cssImport= require("postcss-import");
+    cssImport= require("postcss-import"),
+    browserSync = require("browser-sync").create();
+    
 
 
 
@@ -23,11 +25,23 @@ gulp.task("styles",function(){
 });
 
 gulp.task("watch",function(){
+    
+    browserSync.init({
+        server:{
+            baseDir: "app"
+        }
+    });
+    
     watch("./app/index.html", function(){
-      gulp.start("html") ; 
+      browserSync.reload();
     });
     
     watch("./app/assets/styles/**/*.css", function(){
-        gulp.start("styles");
+        gulp.start("cssInject");
     });
+});
+
+gulp.task("cssInject",["styles"],function(){
+    return gulp.src("./app/assets/styles/styles.css")
+    .pipe(browserSync.stream());
 });
